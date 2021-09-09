f595963ee1eb8c31f8de94d8952b0564b78639a4
Yoon0511 <42472602+Yoon0511@users.noreply.github.com>
2021-09-09 17:50:19 +0900

    Drill03 완성
    2018180001 강민우

---
+++ 02/name.py
@@ -0,0 +1,73 @@
+import turtle
+
+# ㄱ
+turtle.penup()
+turtle.goto(-400,100)
+turtle.pendown()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+#ㅏ
+turtle.penup()
+turtle.goto(-200,150)
+turtle.pendown()
+turtle.forward(150)
+turtle.penup()
+turtle.goto(-200,75)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(50)
+
+# ㅇ
+turtle.penup()
+turtle.goto(-300,-100)
+turtle.pendown()
+turtle.right(90)
+turtle.circle(50)
+
+#ㅁ
+turtle.penup()
+turtle.goto(70,100)
+turtle.pendown()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+# l
+turtle.penup()
+turtle.goto(120,120)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(150)
+
+# ㄴ
+turtle.penup()
+turtle.goto(0,-70)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+# ㅇ
+turtle.penup()
+turtle.goto(300,70)
+turtle.pendown()
+turtle.circle(40)
+
+# ㅜ
+turtle.penup()
+turtle.goto(240,0)
+turtle.pendown()
+turtle.forward(180)
+turtle.left(180)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(100)
+
+
+turtle.exitonclick()
